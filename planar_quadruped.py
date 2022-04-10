5c1d462984b46a3a2e75e7d1ef5ab63f5d1b94a5
Zixin Zhang <1186193932@qq.com>
2022-04-10 00:36:54 -0400

    update planar_quadruped.py

--- planar_quadruped.py
+++ planar_quadruped.py
@@ -8,13 +8,13 @@ class PlanarQuadruped:
         self.t_l = thigh_length
         self.c_l = calf_length
 
-    def dynamics(self, u, t):
+    def dynamics(self, x: np.ndarray, u: np.ndarray, t):
         # x = [x_com, y_com, theta_com], body CoM position and orientation
         # u = [F1, F2], ground reaction forces
         # TODO: return xdot at time t
         return 0.0
 
-    def dynamics_jacobians(self, u, t):
+    def dynamics_jacobians(self, x: np.ndarray, u: np.ndarray, t):
         # we want to get the discrete-time state space model x[k+1] = Ax[k] + Bu[k]
         # TODO: return A and B at time t
         return 0.0
