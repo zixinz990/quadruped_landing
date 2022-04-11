143cdb2270c1102c6f4484a5f68f506386599a92
Zixin Zhang <1186193932@qq.com>
2022-04-11 16:52:28 -0400

    add robot dynamics formula

--- planar_quadruped.py
+++ planar_quadruped.py
@@ -7,12 +7,35 @@ class PlanarQuadruped:
         self.b_l = body_length
         self.t_l = thigh_length
         self.c_l = calf_length
+        self.g = 9.8
 
-    def dynamics(self, x: np.ndarray, u: np.ndarray, t):
-        # x = [x_com, y_com, theta_com], body CoM position and orientation
-        # u = [F1, F2], ground reaction forces
-        # TODO: return xdot at time t
-        return 0.0
+    def dynamics(self, x: np.ndarray, u: np.ndarray, r: np.ndarray, t):
+        """
+        :param x = [x_com, y_com, theta_com, x_com_dot, y_com_dot, theta_com_dot], body CoM position and orientation, column vector with dim 6
+        :param u = [F1, F2], ground reaction forces, column vector with dim 4
+        :param r = [r1, r2], foot positions, column vector with dim 4
+        :return: x_dot = [x_com_dot, y_com_dot, theta_com_dot, x_com_ddot, y_com_ddot, theta_com_ddot], column vector
+        """
+        x_dot = np.zeros((6, 1))
+
+        F_1x = u[0]
+        F_1y = u[1]
+        F_1 = u[0:2]
+
+        F_2x = u[2]
+        F_2y = u[3]
+        F_2 = u[2:4]
+
+        r_1x = r[0]
+        r_1y = r[1]
+        r_2x = r[2]
+        r_2y = r[3]
+
+        x_dot[0:3] = x[3:6]  # x_dot, y_dot, theta_dot 0 1 2
+        x_dot[3:5] = F_1 / self.m + F_2 / self.m + np.array([[0], [self.g]])  # x_ddot, y_ddot 3 4
+        x_dot[5] = (F_1y * r_1x - F_1x * r_1y + F_2y * r_2x - F_2x * r_2y) / (self.m * self.b_l * self.b_l / 12)  # theta_ddot 5
+
+        return x_dot
 
     def dynamics_jacobians(self, x: np.ndarray, u: np.ndarray, t):
         # we want to get the discrete-time state space model x[k+1] = Ax[k] + Bu[k]
--- visualize.py
+++ visualize.py
@@ -1,13 +1,12 @@
 import numpy as np
-import gym 
-
+import gym
 
 if __name__ == "__main__":
-    init_qpos = np.array([0,6,1,0,0,0,0])
+    init_qpos = np.array([0, 6, 1, 0, 0, 0, 0])
     """
     qpos: [x, height, rotation, angles of each joint]
     """
-    env = gym.make("HalfCheetah-v3",exclude_current_positions_from_observation=False, init_qpos = init_qpos)
+    env = gym.make("HalfCheetah-v3", exclude_current_positions_from_observation=False, init_qpos=init_qpos)
     env.reset()
     done = False
     while not done:
