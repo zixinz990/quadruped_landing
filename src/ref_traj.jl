ae2fc02df407b8c989e87a2a263c06cf7bfc406c
Zixin Zhang <1186193932@qq.com>
2022-05-30 17:51:57 -0400

    update ref traj generation

--- src/ref_traj.jl
+++ src/ref_traj.jl
@@ -3,34 +3,31 @@
 
 Return a reference trajectory.
 """
-function reference_trajectory(model::PlanarQuadruped, N, k_trans, xterm, init_mode, dt)
+function reference_trajectory(model::PlanarQuadruped, N, k_trans, xterm, dt)
     n, m = size(model)
 
-    g, mb = model.g, model.mb
-
     Xref = zeros(n, N)
-    Uref = zeros(m, N-1)
+    Uref = zeros(m, N - 1)
 
     # initialize Xref
     Xref[:, 1:end] .= xterm
     Xref[end, 1:end] = range(0, dt * (N - 1), length=N)
 
     # initialize Uref
-    if init_mode == 1
-        Uref[2, 1:k_trans-1] .= -mb * g      # F1_y in contact mode 1
-        
-        Uref[2, k_trans:end] .= -mb * g / 2  # F1_y in contact mode 3
-        Uref[4, k_trans:end] .= -mb * g / 2  # F2_y in contact mode 3
-    else
-        Uref[4, 1:k_trans-1] .= -mb * g      # F2_y in contact mode 2
-        
-        Uref[4, k_trans:end] .= -mb * g / 2  # F2_y in contact mode 3
-        Uref[2, k_trans:end] .= -mb * g / 2  # F1_y in contact mode 3
-    end
-    
-    Uref[5, 1:k_trans-1] .= 0.001  # dt in contact mode 1 or 2
-    Uref[5, k_trans:end] .= 0.02   # dt in contact mode 3
-    # Uref[end, :] .= dt
+    # if init_mode == 1
+    #     Uref[2, 1:k_trans-1] .= -mb * g      # F1_y in contact mode 1
+
+    #     Uref[2, k_trans:end] .= -mb * g / 2  # F1_y in contact mode 3
+    #     Uref[4, k_trans:end] .= -mb * g / 2  # F2_y in contact mode 3
+    # else
+    #     Uref[4, 1:k_trans-1] .= -mb * g      # F2_y in contact mode 2
+
+    #     Uref[4, k_trans:end] .= -mb * g / 2  # F2_y in contact mode 3
+    #     Uref[2, k_trans:end] .= -mb * g / 2  # F1_y in contact mode 3
+    # end
+
+    Uref[end, 1:k_trans-1] .= 0.015  # dt in contact mode 1 or 2
+    Uref[end, k_trans:end] .= 0.02   # dt in contact mode 3
 
     # Convert to a trajectory
     Xref = [SVector{n}(x) for x in eachcol(Xref)]
